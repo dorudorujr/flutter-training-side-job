50787f4e7b58e357da39a088042eeea48f197c14
杉岡成哉 <a1209s518n928@gmail.com>
2025-11-15 16:34:02 +0900

    Textを配置

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,49 @@ class MainApp extends StatelessWidget {
         body: LayoutBuilder(
           builder: (context, constraints) {
             return Center(
-              child: SizedBox(
-                width: constraints.maxWidth / 2,
-                height: constraints.maxWidth / 2,
-                child: const Placeholder(),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: constraints.maxWidth / 2,
+                    height: constraints.maxWidth / 2,
+                    child: const Placeholder(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: constraints.maxWidth / 4,
+                          child: Text(
+                            '** ℃',
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelLarge
+                                ?.copyWith(
+                                  color: Colors.blue,
+                                ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: constraints.maxWidth / 4,
+                          child: Text(
+                            '** ℃',
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelLarge
+                                ?.copyWith(
+                                  color: Colors.red,
+                                ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             );
           },
