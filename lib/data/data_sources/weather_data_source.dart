b61161c0d9bfa4435cf747f6a9dcad7de2a277d3
SeiyaSugiokaPi <seiya_sugioka@post.pioneer.co.jp>
2025-11-29 14:55:45 +0900

    ✨weatherDataSourceProvider作成

---
+++ lib/data/data_sources/weather_data_source.dart
@@ -0,0 +1,25 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
+
+part 'weather_data_source.g.dart';
+
+/// 天気情報を取得するデータソース
+class WeatherDataSource {
+  WeatherDataSource(this._yumemiWeather);
+
+  final YumemiWeather _yumemiWeather;
+
+  /// 天気情報を取得する
+  ///
+  /// [requestJson] リクエストJSON文字列
+  /// 戻り値: レスポンスJSON文字列
+  /// throws: [YumemiWeatherError]
+  String fetchWeather(String requestJson) {
+    return _yumemiWeather.fetchWeather(requestJson);
+  }
+}
+
+@riverpod
+WeatherDataSource weatherDataSource(WeatherDataSourceRef ref) {
+  return WeatherDataSource(YumemiWeather());
+}
---
+++ lib/data/data_sources/weather_data_source.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather_data_source.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$weatherDataSourceHash() => r'7f63244bb39a866ee82087198b7b54216e5d1c7b';
+
+/// See also [weatherDataSource].
+@ProviderFor(weatherDataSource)
+final weatherDataSourceProvider =
+    AutoDisposeProvider<WeatherDataSource>.internal(
+      weatherDataSource,
+      name: r'weatherDataSourceProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$weatherDataSourceHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef WeatherDataSourceRef = AutoDisposeProviderRef<WeatherDataSource>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
