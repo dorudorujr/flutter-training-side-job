1474efaba652a29c5d87bafa30b127b6d284ad02
SeiyaSugiokaPi <seiya_sugioka@post.pioneer.co.jp>
2025-11-30 14:30:38 +0900

    ✨add fetch_weather_use_case.dart

---
+++ lib/domain/use_cases/fetch_weather_use_case.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'fetch_weather_use_case.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$fetchWeatherUseCaseHash() =>
+    r'32c4d62467c369b6136c6a2efb492d71398959da';
+
+/// See also [fetchWeatherUseCase].
+@ProviderFor(fetchWeatherUseCase)
+final fetchWeatherUseCaseProvider =
+    AutoDisposeProvider<FetchWeatherUseCase>.internal(
+      fetchWeatherUseCase,
+      name: r'fetchWeatherUseCaseProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$fetchWeatherUseCaseHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef FetchWeatherUseCaseRef = AutoDisposeProviderRef<FetchWeatherUseCase>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
