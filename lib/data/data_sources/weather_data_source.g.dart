eee4a0527f8b778e696b34cfcd0461b5639c2281
SeiyaSugiokaPi <seiya_sugioka@post.pioneer.co.jp>
2025-11-30 16:23:05 +0900

    ✨add weather_data_source.dart

---
+++ lib/data/data_sources/weather_data_source.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather_data_source.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$weatherDataSourceHash() => r'12c9b08c9c4d53b89bf07b3933ef9525ebad7050';
+
+/// See also [weatherDataSource].
+@ProviderFor(weatherDataSource)
+final weatherDataSourceProvider =
+    AutoDisposeProvider<WeatherDataSource>.internal(
+      weatherDataSource,
+      name: r'weatherDataSourceProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$weatherDataSourceHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef WeatherDataSourceRef = AutoDisposeProviderRef<WeatherDataSource>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
